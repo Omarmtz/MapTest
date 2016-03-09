58446809ae2b2336701c4a57c05d5a20d7369758
Omar Martínez <omarmartinez@Omars-MacBook-Pro.local>
2016-03-09 15:14:52 -0600

    Model Add

--- map/models.py
+++ map/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Point(models.Model):
+    """Point in map"""
+
+    address = models.CharField(blank=True, max_length=500)
+    lattitude = models.DecimalField(max_digits=20, decimal_places=10)
+    altitude = models.DecimalField(max_digits=20, decimal_places=10)
